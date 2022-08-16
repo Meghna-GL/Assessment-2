93c39a71368e07bb3aa0f78f89ef0d5b2ab7f86b
Meghna-GL <meghna.srivastava@globallogic.com>
2022-08-16 11:11:52 +0530

    fix: Customise header

--- src/components/dashBoard.js/Header.js
+++ src/components/dashBoard.js/Header.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom"
 const Header = () => {
     return (
         <>
-            <header>
+            <header id="body-header">
 
                 {/* <nav>
                 <ul>
@@ -19,14 +19,14 @@ const Header = () => {
                     </li>
                 </ul>
             </nav> */}
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <nav className="navbar navbar-expand-lg navbar">
                     {/* <a className="navbar-brand" href="#">Navbar</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button> */}
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
-                            <li className="nav-item active">
+                            <li className="nav-item">
                                 <NavLink to='/' className="nav-link">HOME <span className="sr-only"></span></NavLink>
                             </li>
                             <li className="nav-item">
@@ -39,7 +39,7 @@ const Header = () => {
                             </li>
                         </ul>
 
-                        <div>
+                        <div className="to-right">
                             <ul className="navbar-nav">
                                 <li className="nav-item">
                                     <NavLink to='/login' className="nav-link">LOGIN</NavLink>
