0de2f38a8ca62cfcf6cfb4317c550d20d89d2511
Meghna-GL <meghna.srivastava@globallogic.com>
2022-08-16 10:31:15 +0530

    fix: Replace navbar with bootstrap navbar template and
    ...customised the header as per the requirement

--- src/components/dashBoard.js/Header.js
+++ src/components/dashBoard.js/Header.js
@@ -1,11 +1,11 @@
 import { NavLink } from "react-router-dom"
 
 const Header = () => {
-  return (
-   <>
-    <header>
-            
-            <nav>
+    return (
+        <>
+            <header id="body-header">
+
+                {/* <nav>
                 <ul>
                
                     <li>
@@ -18,13 +18,66 @@ const Header = () => {
                         <NavLink to='/products'>PRODUCTS</NavLink>
                     </li>
                 </ul>
-            </nav>
+            </nav> */}
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    {/* <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button> */}
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav horizontal-list text-left">
+                            <li className="nav-item active">
+                                <NavLink to='/' className="nav-link">HOME <span className="sr-only"></span></NavLink>
+                            </li>
+                            <li className="nav-item">
 
-            
-        </header>
+                                <NavLink to='/about' className="nav-link">ABOUT</NavLink>
+                            </li>
+                            <li className="nav-item">
 
-   </>
-  )
+                                <NavLink to='/products' className="nav-link">PRODUCTS</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+
+            </header>
+
+        </>
+    )
 }
 
 export default Header
+
+
+/*
+
+
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">Navbar</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        // <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        <NavLink to='/' className="nav-link">HOME <span className="sr-only">(current)</span></NavLink>
+      </li>
+      <li className="nav-item">
+        // <a className="nav-link" href="#">Features</a>
+         <NavLink to='/about' className="nav-link">ABOUT</NavLink>
+      </li>
+      <li className="nav-item">
+        //<a className="nav-link" href="#">Pricing</a>
+         <NavLink to='/products' className="nav-link">PRODUCTS</NavLink>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+*/
